80c8f7cceaceb570de55a5d668d623ccb349beea
ZEWEI CHU <zeweichu@gmail.com>
2017-06-24 21:10:14 -0700

    add code

--- Basics/basic.py
+++ Basics/basic.py
@@ -1,8 +1,17 @@
 import sys
 
-
-if __name__ == "__main__":
-	print(sys.argv)
-	print("""this is the main function
-and the second line is here
-you can even write more lines""")
+# this is my first python program aaaaaa
+# this is my first python program aaaaaa
+# this is my first python program aaaaaa
+# this is my first python program aaaaaa
+"""
+	this is my first python program
+	second line
+	third line
+"""
+if __name__ == "__main__": # comments here
+	print("Hello: " + sys.argv[1])
+# 	print("hello world")
+# 	print("""We are studing python at Julyedu,
+# 	and this is the second line
+# and this is the third line""")
